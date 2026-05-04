a89e7d36baf6bd1804728e33889eb8e62f2c2d74
binhr2423 <susclmm@gmail.com>
2026-05-04 19:31:25 +0700

    update

--- src/main/java/com/example/demo/controller/BusinessListingsController.java
+++ src/main/java/com/example/demo/controller/BusinessListingsController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.print.attribute.standard.Media;
 import java.security.Principal;
+import java.util.Objects;
 
 @Controller
 @RequestMapping("/businessListings")
@@ -43,28 +44,48 @@ public class BusinessListingsController {
        p = repo.FindAll(PageRequest.of(start,5));
 
        model.addAttribute("list",p);
+       model.addAttribute("listpicture",mediarepo.findAll());
        model.addAttribute("currentPages",start);
        model.addAttribute("totalPages",p.getTotalPages());
-       model.addAttribute("listings",new listings());
-       model.addAttribute("media",new media());
        model.addAttribute("DTOtranfer",new DTOlistingMedia());
        return "businessListing";
    }
 
     @PostMapping("/createRoom")
     public String createRoom(
-            @ModelAttribute("DTOtransfer") DTOlistingMedia DTO,
+            @ModelAttribute("DTOtranfer") DTOlistingMedia DTO,
             Principal p
     ) {
-        listingsMedia lm = new listingsMedia();
+       listingsMedia lm = new listingsMedia();
+
+        for (listingsMedia a : repo.FindAll()) {
+
+            if (
+                    Objects.equals(a.getM().getId(), DTO.getMedia().getId()) &&
+                            Objects.equals(a.getL().getId(), DTO.getListings().getId())
+            ) {
+                lm = a;
+                System.out.println("FOUND lm id = " + lm.getId());
+                break;
+            }
+        }
+
+        if (lm.getId() == 0) {
+            System.out.println("NOT FOUND, CREATE NEW");
+            lm = new listingsMedia();
+        }
 
         Users u = UserRepo.findByUsername(p.getName());
-
+        System.out.println(lm.id);
         listings listing = DTO.getListings();
         listing.setU(u);
 
         media media =  DTO.getMedia();
+        media.setStatus(0);
         media.setPermission(u.getRole());
+        media.setFileType(media.getUrl().substring(media.getUrl().lastIndexOf(".")));
+        media.setU(u);
+
 
         mediarepo.save(media);
         listingRepo.saveListings(listing);
@@ -72,12 +93,42 @@ public class BusinessListingsController {
         lm.setL(listing);
         lm.setM(media);
 
+
+
         repo.save(lm);
 
         return "redirect:/businessListings/Home";
     }
-//   @GetMapping("")
+   @GetMapping("/updateRoom")
+    public String updateRoom(Model model,@RequestParam("id") int id,@RequestParam(value = "page",defaultValue = "0") int page){
+      int start = Math.max(0,page);
 
+      Page<listingsMedia> p = repo.FindAll(PageRequest.of(start,5));
+      int totalPages = p.getTotalPages();
+
+      if(start >= totalPages) start = 0;
+
+      p = repo.FindAll(PageRequest.of(start,5));
+
+       listingsMedia l = repo.Findbyid(id);
+
+       DTOlistingMedia dto = new DTOlistingMedia();
+       dto.setListings(l.getL());
+       dto.setMedia(l.getM());
+
+      model.addAttribute("list",p);
+      model.addAttribute("listpicture",p);
+      model.addAttribute("currentPages",start);
+      model.addAttribute("totalPages",p.getTotalPages());
+       model.addAttribute("DTOtranfer",dto);
+       return "businessListing";
+   }
+
+   @GetMapping("/deleteRoom")
+    public String deleteRoom(@RequestParam("id") int id){
+      repo.remove(id);
+      return "redirect:/businessListings/Home";
+   }
 
 
 }
